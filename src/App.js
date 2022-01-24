078a1dacb1850ea590024c7c6f04a13e583c94f8
João Vítor Sberse <jotabassosberse@gmail.com>
2022-01-23 22:45:47 -0300

    chakra inicio

--- src/App.js
+++ src/App.js
@@ -2,20 +2,31 @@ import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
 import './App.css';
 import H1 from './components/H1'
 import Home from './components/Home'
+import { Box } from '@chakra-ui/react'
 import { Grid } from '@mui/material';
 
 const App = () => {
   return (
-    <Grid container>
-      <Grid item xs={12} sx={{justifyContent: 'center', textAlign: 'center', alignItems: 'center'}}>
+    <Box bg='#071330' align='center'>
         <Router>
-          <Routes>
-            <Route path="/" element={<H1 />} />
-            <Route path="home" exact element={<Home />} />
-          </Routes>
+            <Routes>
+             <Route path="/" element={<H1 />} />
+              <Route path="home" exact element={<Home />} />
+             </Routes>
         </Router>
-      </Grid>
-    </Grid>
+    </Box>
+
+    // <Grid container>
+    //   <Grid item xs={12} sx={{justifyContent: 'center', textAlign: 'center', alignItems: 'center'}}>
+    //     <Router>
+    //       <Routes>
+    //         <Route path="/" element={<H1 />} />
+    //         <Route path="home" exact element={<Home />} />
+    //       </Routes>
+    //     </Router>
+    //   </Grid>
+    // </Grid>
+
   );
 }
 
--- src/components/H1.js
+++ src/components/H1.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import './H1.scss';
 import t1 from '../../src/t1.jpg'
 import Bt from './Bt'
-
+import { Box } from '@chakra-ui/react';
+import { Image } from '@chakra-ui/react'
 
 function H1 () {
   return (  
-      <header className="App-header">
-        <img src={t1} className="App-logo" alt="t1"/>
-        <p>yoarajota</p>
-        <Bt />
-      </header>
+    <Box h='100%' flexDirection='column' fontSize='calc(10px + 5vmin)' color='#6FABC7'>
+      <Image src={t1} alt="t1" mt='6vw' h='40vwmin' pointerEvents='none' borderRadius='500px'/>
+      <p>yoarajota</p>
+      <Bt />
+    </Box>
   );
 }
 
